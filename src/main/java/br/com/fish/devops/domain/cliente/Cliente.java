a51c4f4aa83b50e26ca0d4503fb443ddc0fae2f4
José Fernando Santos  de Carvalho <jfsc@cin.ufpe.br>
2017-10-28 18:45:56 -0300

    first version

---
+++ src/main/java/br/com/fish/devops/domain/cliente/Cliente.java
@@ -0,0 +1,43 @@
+package br.com.fish.devops.domain.cliente;
+
+public class Cliente {
+
+	private long id;
+
+	private String nome;
+
+	private String email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		Cliente cliente = (Cliente) obj;
+
+		return id == cliente.getId() ? true : false;
+	}
+
+}
---
+++ src/main/java/br/com/fish/devops/domain/pedido/StatusPedido.java
@@ -0,0 +1,17 @@
+package br.com.fish.devops.domain.pedido;
+
+public enum StatusPedido {
+
+	ABERTO("ABERTO"), CONCLUIDO("CONCLUIDO"), CANCELADO("CANCELADO");
+
+	private final String status;
+
+	private StatusPedido(String statusPedido) {
+		status = statusPedido;
+	}
+
+	public String toString() {
+		return this.status;
+	}
+
+}
