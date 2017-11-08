5b690d8cceec2af6ed11be1d1210892e01b99023
rafjordao <rfpj@cin.ufpe.br>
2017-11-08 19:31:19 -0300

    Adicinados projetos bases de Pedido e Cliente

---
+++ Pedido/src/main/java/cin/microsservices/wildcats/pedido/PedidoApplication.java
@@ -0,0 +1,12 @@
+package cin.microsservices.wildcats.pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoApplication.class, args);
+	}
+}
