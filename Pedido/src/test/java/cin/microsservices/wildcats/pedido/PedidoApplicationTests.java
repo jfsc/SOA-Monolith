a40a2a62a2d4e36bef082faf2b7a6ee3e4521943
Carlos Melo <chnm@cin.ufpe.br>
2017-11-12 11:38:55 -0300

    atualiza dependencias de Pedidos

--- Pedido/src/test/java/cin/microsservices/wildcats/pedido/PedidoApplicationTests.java
+++ Pedido/src/test/java/cin/microsservices/wildcats/pedido/PedidoApplicationTests.java
@@ -5,12 +5,24 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import cin.microsservices.wildcats.pedido.domain.pedido.ItemPedido;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PedidoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testCriacaoDePedido() {
+		/**
+		 * Teste simples na criação de um ItemPedido
+		 * 12/11/2017
+		 */
+		//Criação de item pedido
+		ItemPedido item1 = new ItemPedido();
+		item1.setIdProduto(36);
+		item1.setQuantidade(65);
+		
+		
 	}
 
 }
